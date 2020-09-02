38421daf6da6767de1f47e09cdb3a699deb99f7f
Kor1611 <kjv1611@ukr.net>
2020-09-02 23:54:31 +0300

    Lesson 2

--- users-list/src/app/app.component.ts
+++ users-list/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'users-list';
-  typesOfShoes: string[] = ['Smith', 'Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  title = 'Список Пользователей';
+  messages: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
